ee9ea3f10df8dc0551a2504c3e322395af48c1a2
Nando Vieira <fnando.vieira@gmail.com>
2017-05-18 10:48:56 -0700

    More tweaks.

--- test/support/subdomain_tests.rb
+++ test/support/subdomain_tests.rb
@@ -1,9 +1,9 @@
 module SubdomainTests
   def app
-    _tld = tld
+    current_tld = tld
 
     Class.new(App) do
-      set :tld_size, _tld.split(".").size - 1
+      set :tld_size, current_tld.split(".").size - 1
     end
   end
 
@@ -27,42 +27,42 @@ module SubdomainTests
         header "HOST", "mail.example#{tld}"
         get "/"
 
-        assert_equal "any: mail",last_response.body
+        assert_equal "any: mail", last_response.body
       end
 
       test "when any subdomain is required - renders about page" do
         header "HOST", "mail.example#{tld}"
         get "/about"
 
-        assert_equal "any: about mail",last_response.body
+        assert_equal "any: about mail", last_response.body
       end
 
       test "when no subdomain is required - renders root page" do
         header "HOST", "example#{tld}"
         get "/"
 
-        assert_equal "root",last_response.body
+        assert_equal "root", last_response.body
       end
 
       test "when no subdomain is required - renders about page" do
         header "HOST", "example#{tld}"
         get "/about"
 
-        assert_equal "about",last_response.body
+        assert_equal "about", last_response.body
       end
 
       test "when multi-subdomain is required - renders root page" do
         header "HOST", "foo.bar.example#{tld}"
         get "/"
 
-        assert_equal "multiple: foo.bar",last_response.body
+        assert_equal "multiple: foo.bar", last_response.body
       end
 
       test "when multi-subdomain is required - renders about page" do
         header "HOST", "foo.bar.example#{tld}"
         get "/about"
 
-        assert_equal "multiple: about foo.bar",last_response.body
+        assert_equal "multiple: about foo.bar", last_response.body
       end
     end
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-require 'simplecov'
+require "simplecov"
 SimpleCov.start
 
 require "bundler/setup"
