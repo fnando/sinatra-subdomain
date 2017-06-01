057f519b56750461b68d3e98a650aa0086399335
Nando Vieira <fnando.vieira@gmail.com>
2017-06-01 15:38:23 -0700

    Bump up version.

--- lib/sinatra/subdomain/version.rb
+++ lib/sinatra/subdomain/version.rb
@@ -2,8 +2,8 @@ module Sinatra
   module Subdomain
     module Version
       MAJOR = 0
-      MINOR = 2
-      PATCH = 1
+      MINOR = 3
+      PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
   end
